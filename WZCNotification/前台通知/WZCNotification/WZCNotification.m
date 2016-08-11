162eb744c83165ded4cd617af5a2955500b061f1
voisen <voisen@qq.com>
2016-08-11 13:38:26 +0800

    修复了一些 bug

--- WZCNotification/前台通知/WZCNotification/WZCNotification.m
+++ WZCNotification/前台通知/WZCNotification/WZCNotification.m
@@ -28,6 +28,7 @@ static WZCNotification *noti;
         noti = [[WZCNotification alloc]init];
     }
     return noti;
+    
 }
 
 + (void)setDelegate:(id<WZCNotificationDelegate>)delegate{
@@ -54,42 +55,39 @@ static NSDictionary *infoPlist;
         playSystemSound = YES;
         backgroundNotiEnalble = YES;
     });
+    
+    
     if (noti_window) noti_window.hidden = YES;
     noti_window = [[UIWindow alloc]initWithFrame:CGRectMake(0, - Noti_Height,  [UIScreen mainScreen].bounds.size.width, Noti_Height)];
-    
     noti_window.hidden = NO;
-    
     noti_window.backgroundColor = [UIColor colorWithRed:0.180 green:0.125 blue:0.051 alpha:1.00];
-    
     noti_window.windowLevel = UIWindowLevelAlert;
     
     UIImageView *imgView = [[UIImageView alloc]initWithFrame:CGRectMake(15, 5, Noti_Height * 0.34, Noti_Height * 0.34)];
     NSString *iconName = [infoPlist[@"CFBundleIcons"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"] lastObject];
-    
     imgView.image = [UIImage imageNamed:iconName];
     imgView.layer.cornerRadius = 5;
     imgView.layer.masksToBounds = YES;
-    
     [noti_window addSubview:imgView];
     
     NSString *appName = infoPlist[@"CFBundleDisplayName"];
+    
+    if (appName == nil || [appName isEqualToString:@""]) {
+        appName = @"新的消息";
+    }
     UIFont *title_font = [UIFont systemFontOfSize:Font_Size weight:bold];
     CGSize title_size = [appName sizeWithAttributes:@{NSFontAttributeName:title_font}];
     
     UILabel *noti_title = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(imgView.frame) + 5, imgView.center.y - title_size.height / 2.0f, title_size.width, title_size.height)];
-    
     noti_title.font = title_font;
-    
     noti_title.text = appName;
     noti_title.textColor = [UIColor whiteColor];
-    
     [noti_window addSubview:noti_title];
     
     UILabel *title_now = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(noti_title.frame) + 10, noti_title.frame.origin.y,40, noti_title.frame.size.height)];
     title_now.textColor = [UIColor whiteColor];
     title_now.text = @"现在";
     title_now.font = [UIFont systemFontOfSize:Font_Size - 1];
-    
     [noti_window addSubview:title_now];
     
     UILabel *noti_msg = [[UILabel alloc]initWithFrame:CGRectMake(noti_title.frame.origin.x, CGRectGetMaxY(noti_title.frame), noti_window.frame.size.width - noti_title.frame.origin.x - 15, noti_window.frame.size.height - CGRectGetMaxY(imgView.frame) - 7)];
@@ -102,15 +100,17 @@ static NSDictionary *infoPlist;
     [noti_window addGestureRecognizer:tap];
     [noti_window addSubview:noti_msg];
     
-    UIButton *noti_Btn = [UIButton buttonWithType:UIButtonTypeCustom];
     
+    UIButton *noti_Btn = [UIButton buttonWithType:UIButtonTypeCustom];
     noti_Btn.frame = CGRectMake(0, 0, 40, 7);
     noti_Btn.center = CGPointMake(noti_window.center.x, Noti_Height - 9);
     noti_Btn.backgroundColor = [UIColor colorWithRed:0.489 green:0.350 blue:0.251 alpha:1.000];
     noti_Btn.layer.cornerRadius = 3.5f;
     noti_Btn.layer.masksToBounds = YES;
     noti_Btn.tag = 11;
+    noti_Btn.enabled = NO;
     [noti_window addSubview:noti_Btn];
+    
     UISwipeGestureRecognizer *swipe_down = [[UISwipeGestureRecognizer alloc]initWithTarget:[self class] action:@selector(swipe_down:)];
     swipe_down.numberOfTouchesRequired = 1;
     swipe_down.direction = UISwipeGestureRecognizerDirectionDown;
@@ -120,10 +120,13 @@ static NSDictionary *infoPlist;
     swipe_up.numberOfTouchesRequired = 1;
     swipe_up.direction = UISwipeGestureRecognizerDirectionUp;
     [noti_window addGestureRecognizer:swipe_up];
+    
     CGRect frame = noti_window.frame;
     frame.origin.y = 0;
+    
     if (playSystemSound) [[self class] playSystemSound];
     if (backgroundNotiEnalble) [[self class] notification_systemWithTitle:title msg:msg];
+    
     [UIView animateWithDuration:0.3 animations:^{
         noti_window.frame = frame;
     } completion:^(BOOL finished) {
